6839717db97dc75d1c7a83dadbc2aa9048096c25
Carlos-Henrique-Ciencias <halo2mega@outlook.com>
2023-10-20 12:28:10 -0300

    pontos para 1 tipo de dispositivo

--- paginas/teste3.js
+++ paginas/teste3.js
@@ -34,6 +34,15 @@ const Teste3 = ({ navigation, route }) => {
             <TouchableOpacity style={styles.roundButton7} onPress={() => handleButtonPress(6)} />
             <TouchableOpacity style={styles.roundButton8} onPress={() => handleButtonPress(7)} />
             <TouchableOpacity style={styles.roundButton9} onPress={() => handleButtonPress(8)} />
+            <TouchableOpacity style={styles.roundButton10} onPress={() => handleButtonPress(9)} />
+            <TouchableOpacity style={styles.roundButton11} onPress={() => handleButtonPress(10)} />
+            <TouchableOpacity style={styles.roundButton12} onPress={() => handleButtonPress(11)} />
+            <TouchableOpacity style={styles.roundButton13} onPress={() => handleButtonPress(12)} />
+            <TouchableOpacity style={styles.roundButton14} onPress={() => handleButtonPress(13)} />
+            <TouchableOpacity style={styles.roundButton15} onPress={() => handleButtonPress(14)} />
+            <TouchableOpacity style={styles.roundButton16} onPress={() => handleButtonPress(15)} />
+            <TouchableOpacity style={styles.roundButton17} onPress={() => handleButtonPress(16)} />
+            <TouchableOpacity style={styles.roundButton18} onPress={() => handleButtonPress(17)} />
             
             <TouchableOpacity style={styles.navigateButton} onPress={navigateToTest4}>
                 <Text style={styles.navigateButtonText}>Ir para o Final</Text>
@@ -42,7 +51,7 @@ const Teste3 = ({ navigation, route }) => {
     );
 };
 
-const buttonSize = Dimensions.get('window').width * 0.25;
+const buttonSize = Dimensions.get('window').width * 0.10;
 
 const roundButtonBase = {  
     width: buttonSize, 
@@ -62,59 +71,109 @@ const styles = StyleSheet.create({
     },
     roundButton1: {  
         ...roundButtonBase,
-        top: '10%',
-        left: '10%'
+        top: '77%',
+        left: '27%' //baixo pé direito
     },
     roundButton2: {  
         ...roundButtonBase,
-        top: '10%',
-        left: '45%'  // Centrado horizontalmente
+        top: '55%',
+        left: '77%'  // meio pé esquerdo, 1º
     },
     roundButton3: {  
         ...roundButtonBase,
-        top: '10%',
-        right: '10%'  // 10% da direita
+        top: '17%',
+        right: '30%'  // alto pé esquerdo, 1°
     },
     roundButton4: {  
         ...roundButtonBase,
-        top: '45%',  // Centrado verticalmente
+        top: '55%',  // meio pé direito, 1°
         left: '10%'
     },
     roundButton5: {  
         ...roundButtonBase,
-        top: '45%',
-        left: '45%'
+        top: '53%',
+        left: '30%'
     },
     roundButton6: {  
         ...roundButtonBase,
-        top: '45%',
-        right: '10%'
+        top: '21%', //pe esquerdo dedo do meio
+        right: '8%'
     },
     roundButton7: {  
         ...roundButtonBase,
-        bottom: '10%',  // Posicionado a 10% do fundo
-        left: '10%'
+        bottom: '62%',  // pé direito, mindinho
+        left: '0%'
     },
     roundButton8: {  
         ...roundButtonBase,
-        bottom: '10%',
-        left: '45%'
+        bottom: '15%', // baixo direito
+        left: '58%'
     },
     roundButton9: {  
         ...roundButtonBase,
-        bottom: '10%',
-        right: '10%'
+        bottom: '40%',
+        right: '35%'// pe esquerdo, meio, 1°
     },
+    roundButton10: {  
+        ...roundButtonBase,
+        bottom: '62%',
+        right: '0%'// mindinho direito
+    },
+    roundButton11: {  
+        ...roundButtonBase,
+        bottom: '70%',
+        right: '80%'// dedo meio direito
+    },
+    roundButton12: {  
+        ...roundButtonBase,
+        bottom: '76%',
+        right: '62%'// dedao direito
+    },
+    roundButton13: {  
+        ...roundButtonBase,
+        bottom: '52%',
+        right: '4%'// sup 3 direito
+    },
+    roundButton14: {  
+        ...roundButtonBase,
+        bottom: '62%',
+        right: '18%'// sup 2 dir 
+    },
+    roundButton15: {  
+        ...roundButtonBase,
+        bottom: '60%',
+        right: '35%'// sup 1 dir
+    },
+    roundButton16: {  
+        ...roundButtonBase,
+        bottom: '50%',
+        right: '87%'// sup 1 esq
+    },
+    roundButton17: {  
+        ...roundButtonBase,
+        bottom: '58%',
+        right: '75%'// sup 2 esq
+    },
+    roundButton18: {  
+        ...roundButtonBase,
+        bottom: '58%',
+        right: '57%'// sup 3 esq
+    },
+    
     navigateButton: {
         backgroundColor: '#4CAF50',
         padding: 10,
         borderRadius: 5,
-        marginTop: 20,
-        position: 'relative'  // mudamos de 'absolute' para 'relative' para o botão aparecer logo abaixo dos botões redondos
+        marginTop: 550,
+          // mudamos de 'absolute' para 'relative' para o botão aparecer logo abaixo dos botões redondos
+        right: '20%',
+        
     },
     navigateButtonText: {
         color: 'white',
         fontSize: 16
-    }});
+    }
+
+});
 
     export default Teste3;
